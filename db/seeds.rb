7abfcca9c2211c5d91aa599f62849e262b201eac
cristianzaninovic <czaninovic@gmail.com>
2023-08-01 16:36:30 -0400

    db migration and seeding
    Signed-off-by: cristianzaninovic <czaninovic@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+30.times do 
+  Character.create!(name: Faker::Movies::HarryPotter.character,
+                  location: Faker::Movies::HarryPotter.location,
+                  quote: Faker::Movies::HarryPotter.quote,
+                  book: Faker::Movies::HarryPotter.book,
+                  house: Faker::Movies::HarryPotter.house,
+                  spell: Faker::Movies::HarryPotter.spell
+                  )
+end
+
+puts "Seed finished"
+puts "#{Character.count} chars created"
